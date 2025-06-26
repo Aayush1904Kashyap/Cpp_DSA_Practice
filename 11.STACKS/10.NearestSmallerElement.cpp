95da79fcaccb6f52e50faaff4fd08c572a325e6c
AAYUSH <aayushkashyap617@gmail.com>
2025-06-26 18:06:28 +0530

    NearestSmallerElement added

---
+++ 11.STACKS/10.NearestSmallerElement.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ vector<int> NSE(vector<int>& arr){
+    int n=arr.size();
+    vector<int> nge(n);
+    stack<int>st;
+    for(int i=0;i<n;i++){
+        while(!st.empty()&& st.top()>=arr[i]){
+            st.pop();
+
+        }
+        if(st.empty()){
+            nge[i]=-1;
+        }else{
+            nge[i]=st.top();
+        }
+        st.push(arr[i]);
+    }
+    return nge;
+ }
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    vector<int> result = NSE(arr);
+    for (int val : result) {
+        cout << val << " ";
+    }
+    
+    return 0;
+}
